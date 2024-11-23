e2df792ef3851f9b45567881d0a9f509c379b585
KlaudiaT13 <clavelcheval@gmail.com>
2024-11-23 15:16:05 +0100

    setting private and getters in customer

--- src/main/java/backend/Customer.java
+++ src/main/java/backend/Customer.java
@@ -3,12 +3,12 @@ package backend;
 import org.json.JSONObject;
 
 public class Customer {
-    boolean awaitingService;
-    double coordX;
-    double coordY;
-    double destX;
-    double destY;
-    String id;
+    private boolean awaitingService;
+    private double coordX;
+    private double coordY;
+    private double destX;
+    private double destY;
+    private String id;
 
     /**
      * Construct a Customer from a respective json String
@@ -23,5 +23,29 @@ public class Customer {
         destY = object.getDouble("destinationY");
         id = object.getString("id");
         awaitingService = object.getBoolean("awaitingService");
+    }
+
+    public boolean isAwaitingService() {
+        return awaitingService;
+    }
+
+    public double getCoordX() {
+        return coordX;
+    }
+
+    public double getCoordY() {
+        return coordY;
+    }
+
+    public double getDestX() {
+        return destX;
+    }
+
+    public double getDestY() {
+        return destY;
+    }
+
+    public String getId() {
+        return id;
     }
 }
