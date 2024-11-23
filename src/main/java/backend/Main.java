7a21be9d73b75f9f41d56002754876b0e2ffdbf8
matteo <nyce0979@gmail.com>
2024-11-23 12:43:50 +0100

    redesign test

--- src/main/java/backend/Main.java
+++ src/main/java/backend/Main.java
@@ -2,6 +2,6 @@ package backend;
 
 public class Main {
     public static void main(String[] args) {
-        Scenario.chooseScenario("864c6115-d216-44f1-a6d0-0fa3dfccf37b");
+        Scenario.chooseScenario("ahfdiasf-d216-44f1-a6d0-0fa3dfccf37b");
     }
 }
